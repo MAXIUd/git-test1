4abe0e905a3e65ef4350f0866b276a13f933a731
MAXIUD <1990783877@qq.com>
2018-03-09 17:11:33 +0800

    My first gitjhkhkjh

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,14 @@ public class User {
     private Integer id;
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Integer getId() {
         return id;
     }
